0a42453dd7cab56b1689ce38ed8f5e553d145ca9
Dmytro Anikin <dimianni@users.noreply.github.com>
2020-04-21 18:28:45 +0200

    Updates

---
+++ precache-manifest.e14e739a454a4de7d82a9fe34a9553a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a96e9ef6b241085a653af94efa112d0",
+    "url": "/outspoken/index.html"
+  },
+  {
+    "revision": "202a0e4a9cd521ade108",
+    "url": "/outspoken/static/css/main.583cf1a7.chunk.css"
+  },
+  {
+    "revision": "65251b531c25a2c4ea4e",
+    "url": "/outspoken/static/js/2.1ca86cdb.chunk.js"
+  },
+  {
+    "revision": "b76313f5190299de114525a0970f69c7",
+    "url": "/outspoken/static/js/2.1ca86cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202a0e4a9cd521ade108",
+    "url": "/outspoken/static/js/main.c157a2cb.chunk.js"
+  },
+  {
+    "revision": "1f6ef14027a622e06590",
+    "url": "/outspoken/static/js/runtime-main.2fb74bb6.js"
+  },
+  {
+    "revision": "70635d725d711fb482cdf8d9eb18edf1",
+    "url": "/outspoken/static/media/Demo.70635d72.ttf"
+  },
+  {
+    "revision": "561896752462737112c830dcbb89ca18",
+    "url": "/outspoken/static/media/Outspoken.56189675.svg"
+  }
+]);
